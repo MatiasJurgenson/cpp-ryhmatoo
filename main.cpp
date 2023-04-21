f258a983d6a69d1f8c0ceb6c91886285de79087f
Marcusksi <marcus.vessel@gmail.com>
2023-04-21 18:46:32 +0300

    ‘käsurida’

--- main.cpp
+++ main.cpp
@@ -1,17 +1,26 @@
 #include "funktsioonid.h"
 
 #include <iostream>
+#include <vector>
 
 //programmi jooksutamiseks tee "make roulette"
 
-int main() {
+int main(int argc, char* argv[]) {
     int konto = 1000;
     int* kontoptr = &konto;
 
-    for (int i = 0; i < 10; i++) {
-        round(100, 21, kontoptr);
+   
+    std::vector<int> kasutaja_sisend; //käsurea sisendi hoidmiseks
+
+    for (int i = 0; i < argc; ++i) { //töötleb käsurea sisendit
+        kasutaja_sisend.push_back(std::atoi(argv[i])); 
     }
 
+    if (argc == 3) { //kui käsurealt on sisestatud 2 arvu, siis kasutab neid, et rouletti mängida
+        round(kasutaja_sisend.at(1), kasutaja_sisend.at(2), kontoptr);
+    }
+    
+    
     std::cout << konto << "\n";
 
     return 0;
